e1a37b1f11e6a8ca9c9076c04b1794f780e711df
Mukul Pradhan <mukul.bhardwaj@ethara.ai>
2026-05-16 18:36:42 +0530

    fix vite preview host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['team-task-manager-production-8ed4.up.railway.app'],
+    host: '0.0.0.0',
+    port: 8080,
+  }
 })
